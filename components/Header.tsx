17602470517d89b2724ec5ed9ab3d30b8d445ec5
Amrindra <amrindraouk@yahoo.com>
2022-04-20 19:39:16 -0400

    added some styles to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,19 @@
 function Header() {
   return (
     <div>
-      <div>
-        {' '}
+      <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
           width={100}
           height={100}
           className="cursor-pointer object-contain"
         />
-        <ul>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>New & Popular</li>
-          <li>My List</li>
+        <ul className="hidden  md:flex  space-x-4">
+          <li className="header-link">Home</li>
+          <li className="header-link">TV Shows</li>
+          <li className="header-link">Movies</li>
+          <li className="header-link">New & Popular</li>
+          <li className="header-link">My List</li>
         </ul>
       </div>
       <div></div>
