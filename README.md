7fd2f4e323d18bf7b7663c1d0730237b6dbbb0ec
Amrindra <amrindraouk@yahoo.com>
2022-04-20 19:09:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-comamrindranetflix-clone-git
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-comamrindranetflix-clone-git)
---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+function Header() {
+  return (
+    <div>
+      <div>
+        {' '}
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Header;
