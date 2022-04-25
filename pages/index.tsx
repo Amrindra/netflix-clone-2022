4c3c1ee9a9b7fd5402a63010704e7c557173da38
Amrindra <amrindraouk@yahoo.com>
2022-04-25 19:37:49 -0400

    desgined Banner component

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
-  console.log(netflixOriginals);
+  // console.log(netflixOriginals);
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
