661dc2d2038dc0d4a2a3aece59b23eebdba7a4d2
Amrindra <amrindraouk@yahoo.com>
2022-04-28 18:57:16 -0400

    Added images to Login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,13 +40,13 @@ const Home = ({
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
         {/* we cannot do the server side redering of the component, we can only do in pages. so we have to pass api data as props  */}
         <Banner netflixOriginals={netflixOriginals} />
-        
+
         <section className="md:space-y-24">
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
-          {/* My List */}
 
+          {/* My List */}
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />
           <Row title="Romance Movies" movies={romanceMovies} />
@@ -93,16 +93,3 @@ export const getServerSideProps = async () => {
     },
   };
 };
-
-// return {
-//   props: {
-//     netflixOriginals: netflixOriginals.results ? netflixOriginals : null,
-//     trendingNow: trendingNow.results ? trendingNow : null,
-//     topRated: topRated.results ? topRated : null,
-//     actionMovies: actionMovies.results ? actionMovies : null,
-//     comedyMovies: comedyMovies.results ? comedyMovies : null,
-//     horrorMovies: horrorMovies.results ? horrorMovies : null,
-//     romanceMovies: romanceMovies.results ? romanceMovies : null,
-//     documentaries: documentaries.results ? documentaries : null,
-//   },
-// };
